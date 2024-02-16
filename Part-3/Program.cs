279860ac6330d12fd4069da14079e0a5d866f89d
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-02-16 17:46:31 +0530

    Part-3

---
+++ Part-3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Program
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var number =new int[3];
+            numbers[0]=1;
+            Console.WriteLine(numbers[0])
+        }
+    }
+} 
