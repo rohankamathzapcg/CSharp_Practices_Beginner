244a56737ae05775667e57eaf3ac5de454c93aea
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-02-16 16:58:46 +0530

    Part-2

---
+++ Part-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Program
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public void Introduce()
+        {
+            Console.WriteLine("My Name is " + FirstName + " " + LastName);
+        }
+    }
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var john=new Person();
+            john.FirstName="John";
+            john.LastName="Smith";
+            john.Introduce();
+        }
+    }
+}
